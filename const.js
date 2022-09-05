93643abb3e7a269438a034d13a01a7ab20d433c9
DESKTOP-1L7397I\82105 <w2117@e-mirim.hs.kr>
2022-09-05 19:30:08 +0900

    자바스크립트 기초01

---
+++ const.js
@@ -0,0 +1,10 @@
+// const 선언자
+// 선언 시점에 할당한 값을 절대로 변경하지 않는 용도
+const x = 5; //변수 x에 5를 저장 
+const y = 6; //변수 y에 6을 저장 
+const z = x + y; //변수 z에 변수 x와 변수 y를 더한 값을 저장. z에는 11이 저장됨
+
+// x = 7; //에러 발생 
+// const x = 7; //에러 발생 
+
+const dateFormat = 'yyy.MM.dd';
