2c2ac7db74f47d483b0b3d9e07590a679cc0a9ba
DESKTOP-1L7397I\82105 <w2117@e-mirim.hs.kr>
2022-09-06 23:58:23 +0900

    map

--- 자바스크립트_기초/Array내장함수/map.js
+++ 자바스크립트_기초/Array내장함수/map.js
@@ -1,1 +1,28 @@
 // map() 함수는 배열의 데이터가 Object형일 때, 배열에 담긴 Object를 새로운 형태의 Object로 변환해서 배열로 반환
+let userList = [{
+    firstName : "재석",
+    lastName : "유",
+    email: "yu@gmail.com"
+},{
+    firstName : "종국",
+    lastName : "김",
+    email: "kim@gmail.com"
+},{
+    firstName : "세찬",
+    lastName : "양",
+    email: "yang@gmail.com"
+},{
+    firstName : "석진",
+    lastName : "지",
+    email: "ji@gmail.com"
+},
+];
+let userList2 = userList.map(function(user){
+    return{
+        firstName: user.lastName + user.firstName,
+        firstName: user.firstName,
+        lastName: user.lastName,
+        email : user.email
+
+    }
+});
