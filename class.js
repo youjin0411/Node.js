17bef87c70178eeb9b3648a215eb2628f6a00e8a
DESKTOP-1L7397I\82105 <w2117@e-mirim.hs.kr>
2022-09-07 00:10:36 +0900

    class

---
+++ class.js
@@ -0,0 +1,19 @@
+Class Car {
+    constructor(modelName, modelYear, type, price){
+        this.modelName = modelName;
+        this.modelYear = modelYear;
+        this.type = type;
+        this.price = price;
+    }
+    getModelName(){
+        return this.modelName;
+    }
+    getModelYear(){
+        return this.modelYear;
+    }
+    getType(){
+        return this.type;
+    }
+}
+let ionic = new Car("아이오닉","2021","e", 4000);
+let genesis = new Car("제네시스", "2021", "g", 7000);
