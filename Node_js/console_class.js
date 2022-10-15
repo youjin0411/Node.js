f3117df2f57371b4a1764cd2cd50ceb35b636ea8
DESKTOP-1L7397I\82105 <w2117@e-mirim.hs.kr>
2022-10-15 22:54:31 +0900

    server 공부 console class

---
+++ Node_js/console_class.js
@@ -0,0 +1,2 @@
+const fs = require('fs'); //fs는 파일시스템 모듈로 파일 읽기, 스기 등을 할 수 있는 내장 모듈
+const {Console} = require('console'); //console 모듈 사용 
--- Node_js/module.js
+++ Node_js/module.js
@@ -1,5 +1,6 @@
 const {add, minus, defaultNum} = require('./calculator');
-
+//모듈로 만들어진 자바스크립트 파일은 require() 함수를 통해서 가져올 수 있다. 
+//require() 함수의 경로를 전달한다. 파일 경로에서 자바스크립트 확장자인 .js는 생략해도 된다. 
 console.log(add(7,2));
 console.log(minus(7,2));
 console.log(defaultNum);
